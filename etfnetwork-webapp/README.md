a1aaa560d26b8b13b80f831e4985d2b772f19fe6
Vikas <vikascjadhav@gmail.com>
2018-01-18 02:40:05 +0530

    First Commit for ETFNetwork WebApp

---
+++ etfnetwork-webapp/README.md
@@ -0,0 +1,9 @@
+# ETF Network WebApp
+## Quick Start
+
+  
+
+$ cd etfnetwork-webapp
+$ npm install
+$ npm start
+```
---
+++ etfnetwork-webapp/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'ETF Network App' });
+});
+
+module.exports = router;
