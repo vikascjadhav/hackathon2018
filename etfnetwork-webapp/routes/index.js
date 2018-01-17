5adc5efa7af45823556be51b8682e84ea53d618b
Vikas <vikascjadhav@gmail.com>
2018-01-18 03:07:46 +0530

    Layout Updated

--- etfnetwork-webapp/routes/index.js
+++ etfnetwork-webapp/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var Client = require('node-rest-client').Client;
+ 
+var client = new Client();
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ETF Network App' });
+	client.get("http://localhost:3000/api/Client", function (data, response) {
+    	console.log(data);
+	});
+
+   res.render('index', { title: 'ETF Network App' });
 });
 
+//http://localhost:3000/api/Client
 module.exports = router;
